ff4c92fe7de53e3ebb32617cb3285f5f4664eff4
Harish Mohan Raj <harish@airt.ai>
2025-01-20 06:35:11 +0000

    Add quotes around each packahe name

--- scripts/docs_build.sh
+++ scripts/docs_build.sh
@@ -39,15 +39,15 @@ install_graphviz() {
 
 install_packages() {
     pip install -e ".[docs]"
-    pip install falkordb graphrag_sdk neo4j-graphrag pypdf \
-        pdoc3>=0.11.5 \
-        pyyaml>=6.0.2 \
-        termcolor>=2.5.0 \
-        nbclient>=0.10.2 \
-        arxiv>=2.1.3 \
+    pip install "falkordb" "graphrag_sdk" "neo4j-graphrag" "pypdf" \
+        "pdoc3>=0.11.5" \
+        "pyyaml>=6.0.2" \
+        "termcolor>=2.5.0" \
+        "nbclient>=0.10.2" \
+        "arxiv>=2.1.3" \
         "flaml[automl]" \
-        pygraphviz>=1.14 \
-        replicate
+        "pygraphviz>=1.14" \
+        "replicate"
 }
 
 docs_generate() {
