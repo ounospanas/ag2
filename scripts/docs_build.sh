4cd3d84e68fce88111635fd2188f31fa19727315
Harish Mohan Raj <harish@airt.ai>
2025-01-20 05:56:05 +0000

    Fix review comments

--- scripts/docs_build.sh
+++ scripts/docs_build.sh
@@ -11,22 +11,21 @@ install_graphviz_mac() {
         echo "Error: Homebrew is not installed. Please install Homebrew first."
         echo "You can install Homebrew by running:"
         echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
-        return 1
+        exit 1
     fi
 }
 
 install_graphviz_ubuntu() {
     if command -v apt-get >/dev/null 2>&1; then
         echo "Installing Graphviz on Ubuntu..."
-        sudo apt-get update
+        sudo apt-get update && \
         sudo apt-get install -y graphviz graphviz-dev
     else
         echo "Error: apt-get not found. Are you sure this is a Debian/Ubuntu system?"
-        return 1
+        exit 1
     fi
 }
 
-# Detect OS and run appropriate installation function
 install_graphviz() {
     if [[ "$OSTYPE" == "darwin"* ]]; then
         install_graphviz_mac
@@ -34,21 +33,35 @@ install_graphviz() {
         install_graphviz_ubuntu
     else
         echo "Unsupported operating system"
-        return 1
+        exit 1
     fi
 }
 
-# Function to build documentation
-docs_build() {
-    install_graphviz
+install_packages() {
     pip install -e ".[docs]"
-    pip install falkordb graphrag_sdk neo4j-graphrag pypdf && \
-        cd website && \
+    pip install falkordb graphrag_sdk neo4j-graphrag pypdf \
+        pdoc3>=0.11.5 \
+        pyyaml>=6.0.2 \
+        termcolor>=2.5.0 \
+        nbclient>=0.10.2 \
+        arxiv>=2.1.3 \
+        "flaml[automl]" \
+        pygraphviz>=1.14 \
+        replicate
+}
+
+docs_generate() {
+    cd website && \
         python ./process_api_reference.py && \
         python ./process_notebooks.py render
 }
 
-# Execute the function only if the script is run directly
+docs_build() {
+    install_graphviz && \
+        install_packages && \
+        docs_generate
+}
+
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     docs_build
 fi
